bfd4d43bc1f828fd543a31bebcfabf0f58c2819d
DmitryBurnaev <dmitry.burnaev@gmail.com>
2019-10-02 12:10:34 +0300

    several changes for project
    - updated package info

---
+++ yandex_disk_client/__init__.py
@@ -0,0 +1,8 @@
+"""
+Just simple python SDK for YandexDisk
+"""
+__title__ = 'yandex_disk_client'
+__version__ = '0.0.1'
+__author__ = 'Dmitry Burnaev'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2019 Dmitry Burnaev'
